89d03a7b5bc011a4803bf5608e335639efb043d9
Jobert Tunguia <163331682+urTeds@users.noreply.github.com>
2025-01-19 11:02:30 +0800

    Group 2 ECommerce

---
+++ Group 2 ECommerce/Code/ecommerce website/about.php
@@ -0,0 +1,195 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<section class="about">
+
+   <main class="main">
+      <h3>ABOUT US</h3>
+
+      <p>Welcome to Bombee – Your Ultimate Online Shopping Destination!
+
+      At Bombee, we believe shopping should be easy, exciting, and tailored to you. Our curated selection of products brings you the latest trends, top-rated essentials, and unique finds – all in one place. Whether you're looking for fashion, gadgets, home decor, or something special, Bombee offers a seamless shopping experience with secure payment options and fast delivery.</p>
+   </main>
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/about-img.svg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>Why choose us?</h3>
+         <p>Choose us because we are the best!</p>
+         <a href="contact.php" class="btn">Contact Us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+   
+   <h1 class="heading">Client's Reviews</h1>
+
+   <div class="swiper reviews-slider">
+
+   <div class="swiper-wrapper">
+
+      <div class="swiper-slide slide">
+         <img src="images/rita.jpg" alt="">
+         <p>I like that it is user friendly and i could understand what I should click based on the format of the website!</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Rita Mae Estimo</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/kat.jpg" alt="">
+         <p>Looking very good and very nice.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Kathy Joy Maghalin</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/michelle.jpeg" alt="">
+         <p>Very nice</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Michelle De Los Santos</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/jobert.JPG" alt="">
+         <p>I like the User Interface</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Jobert tunguia</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/engelica.jpg" alt="">
+         <p>I like how easy it is for me to place and find the products that i would like to buy.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Engelica Getalado</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/shrek.jpg" alt="">
+         <p>Arf Arf Arf</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+         </div>
+         <h3>Shrek</h3>
+      </div>
+
+   </div>
+
+   <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      0: {
+        slidesPerView:1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+</script>
+
+</body>
+</html>
